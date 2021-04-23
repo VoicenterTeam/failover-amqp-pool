e2e11c021ce5ad182b9dbeb65842e45c8ae6747f
Maksym Kyrylov <kirillovmax@gmail.com>
2021-04-23 14:43:07 +0300

    Multiple simultaneous connections

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # failover-amqp-pool
 amqp pool client
 
-### Configuration
+### Usage
+
+It is important to set correct `directives` to make pool all queues and exchanges bind properly like `"directives": "ae,aq,qte"`.
+- ae - assert exchang
+- aq - assert queue
+- qte - bind queue to exchange
+
+### Consume
+config:
 ```json
 [
   {
@@ -15,108 +23,203 @@ amqp pool client
       "heartbeat": 5
     },
     "channel": {
-      "exchange_name": "TestExchange",
-      "queue_name": "TestQueue",
-      "prefetch": 5,
+      "directives": "ae,aq,qte",
+      "exchange_name": "TestE",
       "exchange_type": "fanout",
+      "exchange_durable": true,
+      "queue_name": "TestQ",
+      "queue_durable": true,
+      "queue_exclusive": false,
+      "prefetch": 5,
       "topic": "",
       "options": {}
     }
   },
   {
     "connection": {
-      "host": "127.0.0.2",
+      "host": "127.0.0.1",
       "port": 5672,
       "ssl": false,
-      "username": "user",
+      "username": "test",
       "password": "password",
       "vhost": "/",
       "heartbeat": 5
     },
     "channel": {
+      "directives": "ae,aq,qte",
       "exchange_name": "TestExchange",
-      "queue_name": "TestQueue",
-      "prefetch": 5,
       "exchange_type": "fanout",
+      "exchange_durable": true,
+      "queue_name": "TestQueue",
+      "queue_durable": true,
+      "queue_exclusive": false,
+      "prefetch": 5,
       "topic": "",
       "options": {}
     }
   },
   {
     "connection": {
-      "host": "127.0.0.3",
+      "host": "127.0.0.1",
       "port": 5672,
       "ssl": false,
-      "username": "user",
+      "username": "test",
       "password": "password",
       "vhost": "/",
       "heartbeat": 5
     },
     "channel": {
-      "exchange_name": "TestExchange",
-      "queue_name": "TestQueue",
-      "prefetch": 5,
+      "directives": "ae,aq,qte",
+      "exchange_name": "TestExchange1",
       "exchange_type": "fanout",
+      "exchange_durable": true,
+      "queue_name": "TestQueue1",
+      "queue_durable": true,
+      "queue_exclusive": false,
+      "prefetch": 5,
       "topic": "",
       "options": {}
     }
   },
   {
     "connection": {
-      "host": "127.0.0.4",
+      "host": "127.0.0.1",
       "port": 5672,
       "ssl": false,
-      "username": "user",
+      "username": "test",
       "password": "password",
       "vhost": "/",
       "heartbeat": 5
-
     },
     "channel": {
-      "exchange_name": "TestExchange",
-      "queue_name": "TestQueue",
-      "prefetch": 5,
+      "directives": "ae,aq,qte",
+      "exchange_name": "TestExchange2",
       "exchange_type": "fanout",
+      "exchange_durable": true,
+      "queue_name": "TestQueue2",
+      "queue_durable": true,
+      "queue_exclusive": false,
+      "prefetch": 5,
       "topic": "",
       "options": {}
     }
   }
 ]
 ```
-
-### Usage
-consume:
+code:
 ```js
-let cfg = require('./config.json');
-let Pool = require('./index');
-
-let client = new Pool(cfg);
-
-client.on('channel', (channel) => {
-  channel.consume();
-  channel.on('message', (message) => {
-    console.log("Published ------ " + message.content.toString());
-    client.ack(message);
-  });
+let cfg = require('./config1.json');
+const AMQPPool = require('./index');
+let i = 0;
+let pool = new AMQPPool(cfg);
+pool.start();
+pool.on('ready', (_channel) => {
+  (function (channel) {
+    channel.on("message", (message) => {
+      setTimeout(() => {
+        console.log('<< '+message.content.toString() + " -> " + i);
+        channel.ack(message);
+        i++;
+      }, 500);
+    });
+    channel.consume();
+  })(_channel);
 });
-
-client.connect();
 ```
 
-feed:
+### Feed
+config:
+```json
+[
+  {
+    "connection": {
+      "host": "127.0.0.1",
+      "port": 5672,
+      "ssl": false,
+      "username": "user",
+      "password": "password",
+      "vhost": "/",
+      "heartbeat": 5
+    },
+    "channel": {
+      "directives": "ae",
+      "exchange_name": "TestE",
+      "exchange_type": "fanout",
+      "exchange_durable": true,
+      "topic": "",
+      "options": {}
+    }
+  },
+  {
+    "connection": {
+      "host": "127.0.0.1",
+      "port": 5672,
+      "ssl": false,
+      "username": "test",
+      "password": "password",
+      "vhost": "/",
+      "heartbeat": 5
+    },
+    "channel": {
+      "directives": "ae",
+      "exchange_name": "TestExchange",
+      "exchange_type": "fanout",
+      "exchange_durable": true,
+      "topic": "",
+      "options": {}
+    }
+  },
+  {
+    "connection": {
+      "host": "127.0.0.1",
+      "port": 5672,
+      "ssl": false,
+      "username": "test",
+      "password": "password",
+      "vhost": "/",
+      "heartbeat": 5
+    },
+    "channel": {
+      "directives": "ae",
+      "exchange_name": "TestExchange1",
+      "exchange_type": "fanout",
+      "exchange_durable": true,
+      "topic": "",
+      "options": {}
+    }
+  },
+  {
+    "connection": {
+      "host": "127.0.0.1",
+      "port": 5672,
+      "ssl": false,
+      "username": "test",
+      "password": "password",
+      "vhost": "/",
+      "heartbeat": 5
+    },
+    "channel": {
+      "directives": "ae",
+      "exchange_name": "TestExchange2",
+      "exchange_type": "fanout",
+      "exchange_durable": true,
+      "topic": "",
+      "options": {}
+    }
+  }
+]
+```
+code:
 ```js
 let cfg = require('./config.json');
-let Pool = require('@voicenter-team/failover-amqp-pool');
+const AMQPPool = require('./index');
+let i = 0;
 
-let client = new Pool(cfg);
+let pool = new AMQPPool(cfg);usage
+pool.start();
 
-client.on('channel', (channel) => {
-  setInterval(() => {
-    let msg = 'Hello World ' + Math.random();
-    console.log("Publishing ------ " + msg);
-    channel.publish(msg);
-  }, 1000);
-});
-
-client.connect();
+setInterval(() => {
+  pool.publish("Mesage-" + i, "rr");
+  i++;
+}, 1000);
 ```
--- index.js
+++ index.js
@@ -1,236 +1,104 @@
-const amqplib = require('amqplib');
+function _parsConfig (rawConfig) {
+  let parsedConfig = {};
+  for (let item in rawConfig) {
+    let url = _buildUrl(rawConfig[item].connection);
+    parsedConfig[url] = (parsedConfig.hasOwnProperty(url)) ? parsedConfig[url] : [];
+    parsedConfig[url].push(rawConfig[item].channel);
+  }
+  // console.log(parsedConfig)
+  return parsedConfig;
+}
+
+function _buildUrl(config) {
+  let url = (config.ssl ? 'amqps' : 'amqp') + '://';
+  if (config.username && config.password) {
+    url += config.username + ':' + config.password + '@';
+  }
+  url += config.host + ':' + config.port;
+  if (config.vhost) {
+    url += config.vhost;
+  }
+  if(config.hasOwnProperty('heartbeat')) {
+    url += '?heartbeat=' + config.heartbeat;
+  }
+  return url;
+}
+
+const Connection = require('./Connection');
+const Channel = require('./Channel');
 const EventEmitter = require('events').EventEmitter;
 
-class AmqpPool extends EventEmitter {
-  constructor(config) {
+class AMQPPool extends EventEmitter {
+  constructor(rawConfig) {
     super();
 
-    this.configPool = [];
-    this.msgPool = [];
-    this.currentConfigIndex = 0;
-    this.currentConfig = null;
-    this.currentConnection = false;
-    this.currentChannel = false;
+    this.config = _parsConfig(rawConfig);
+    this.connections = [];
 
-    if (config instanceof Array) {
-      this.configPool = config;
-    } else {
-      this.configPool = [config];
-    }
+    // round_robin
+    this.rr_i = 0;
   }
 
-  connect() {
-    setTimeout(() => {
-      if (this.configPool.length > 0) {
-        let config = this._getCurrentConfig();
-        if (config && config.hasOwnProperty('connection')) {
-          return this._startConnection()
-            .then(this._startChannel.bind(this))
-            .then(this._configChannel.bind(this))
-            .catch(this._handleError.bind(this));
+  start() {
+    for (let _url in this.config) {
+      ((url) => {
+        let connection = new Connection(url);
+        this.connections.push(connection);
+        for (let channelConfigIndex in this.config[url]) {
+          let channel = new Channel(connection, this.config[url][channelConfigIndex]);
+          channel.on('ready', (channel) => {
+            this.emit('ready', channel);
+          });
+          channel.on('message', (msg) => {
+            this.emit('message', msg);
+          });
         }
-      }
-      // this._tryRecover();
-    }, 1000);
-  }
-
-  close() {
-    return this.currentConnection.close();
-  }
-
-  consume(_r = false) {
-    if (this.currentChannel) {
-      if (!_r) {
-        this.removeAllListeners('message');
-      }
-      let config = this._getCurrentConfig();
-      this.currentChannel.consume(config.channel.queue_name, (m) => {
-        if (m == null) {
-          this.consume(1);
-        } else {
-          this.emit('message', m);
-        }
-      });
-    }
-  }
-
-  publish(message = null) {
-    console.log(this.msgPool);
-    message = message||this.msgPool.shift();
-    if (message) {
-      let config = this._getCurrentConfig();
-      if (config.channel.hasOwnProperty('exchange_name')) {
-        if (this.currentChannel) {
-          let msg = Buffer.from(message);
-          this.currentChannel.publish(config.channel.exchange_name, config.channel.topic || '', msg, config.channel.options || {});
-          this.publish();
-        }  else {
-          this.msgPool.push(message);
-        }
-      }
-    }
-  }
-
-  sendToQueue(message) {
-    if (this.currentChannel) {
-      let config = this._getCurrentConfig();
-      let msg = Buffer.from(message);
-      if (config.channel.hasOwnProperty('queue_name')) {
-        this.currentChannel.sendToQueue(config.channel.queue_name, msg);
-      }
-    }
-  }
-
-  ack(m) {
-    if (this.currentChannel) {
-      this.currentChannel.ack(m);
-    }
-  }
-
-  nack(m) {
-    if (this.currentChannel) {
-      this.currentChannel.nack(m);
-    }
-  }
-
-  _tryRecover() {
-    this._rotateConfig();
-    this.connect();
-  }
-
-  _getCurrentConfig() {
-    if (!this.currentConfig) {
-      this.currentConfig = this.configPool[this.currentConfigIndex];
-    }
-    return this.currentConfig;
-  }
-
-  _rotateConfig() {
-    this.currentConfigIndex++;
-    if (this.configPool.length > 0) {
-      if (this.currentConfigIndex >= this.configPool.length) {
-        this.currentConfigIndex = 0;
-      }
-      this.currentConfig = this.configPool[this.currentConfigIndex];
-      return this.currentConfigIndex;
-    }
-    else {
-      return false;
-    }
-  }
-
-  _buildUrl(config) {
-    let url = (config.ssl ? 'amqps' : 'amqp') + '://';
-    if (config.username && config.password) {
-      url += config.username + ':' + config.password + '@';
-    }
-    url += config.host + ':' + config.port;
-    if (config.vhost) {
-      url += config.vhost;
-    }
-    if(config.hasOwnProperty('heartbeat')) {
-      url += '?heartbeat=' + config.heartbeat;
-    }
-    return url;
-  }
-
-  _startConnection() {
-    let config = this._getCurrentConfig();
-    let url = this._buildUrl(config.connection);
-    return amqplib.connect(url, {})
-      .then((connection) => {
-        connection.on('error', (e) => {
-          console.log("Connection Error");
-          console.log(e);
-        });
         connection.on('close', () => {
-          console.log('Close Connection');
-          this.currentConnection = false;
-          this.currentChannel = false;
-          this._tryRecover();
+          console.log('close ' + url);
+          this.connections.splice(this.connections.indexOf(connection), 1);
+          setTimeout(() => {
+            connection.start();
+          }, 500);
         });
-        this.currentConnection = connection;
-        return connection;
-      });
-  }
-
-  _startChannel(connection) {
-
-    if (!connection) {
-      return Promise.reject('No connection');
+        connection.start();
+      })(_url);
     }
-
-    return connection.createConfirmChannel()
-      .then((channel) => {
-        channel.on('error', (e) => {
-          console.log("Channel Error");
-          console.log(e);
-        });
-        channel.on('close', () => {
-          console.log("Channel Close");
-          this.close();
-        });
-        return channel;
-      })
-      .then((channel) => {
-        this.currentChannel = channel;
-        return channel;
-      });
   }
 
-  _configChannel(channel) {
-
-    if (!channel) {
-      return Promise.reject('No channel');
+  publish(msg, filter) {
+    let channels = this.getAllChannels();
+    if (typeof filter == 'function') {
+      // console.log(this.connections);
+      filter.call(this, msg, channels);
+    } else if (filter === 'rr') {
+      if (channels.length > 0 ) {
+        if(this.rr_i >= channels.length) {
+          this.rr_i = 0;
+        }
+        console.log(channels[this.rr_i].exchange);
+        channels[this.rr_i++].publish(msg);
+      }
+    } else if (filter === 'all') {
+      for (let channelIndex in channels) {
+        console.log(channels[channelIndex].exchange);
+        channels[channelIndex].publish(msg);
+      }
     }
-
-    return Promise.resolve(channel)
-      .then((channel) => {
-        let config = this._getCurrentConfig();
-        if (config.channel.hasOwnProperty('exchange_name')) {
-          return channel.assertExchange(config.channel.exchange_name, config.channel.exchange_type, {durable: true})
-            .then(() => {
-              return channel;
-            });
-        }
-        return channel;
-      })
-      .then((channel) => {
-        let config = this._getCurrentConfig();
-        if (config.channel.queue_name !== false) {
-          return channel.assertQueue(config.channel.queue_name || '', {exclusive: false, durable: true, noAck: !config.channel.prefetch})
-            .then((assertion) => {
-              if (config.channel.hasOwnProperty('exchange_name')) {
-                return channel.bindQueue(assertion.queue, config.channel.exchange_name, config.channel.topic || '', config.channel.options || {})
-                  .then(() => {
-                    this.currentConfig.channel.queue_name = assertion.queue;
-                    return channel;
-                  });
-              }
-              return channel;
-            });
-        }
-        return channel;
-      })
-      .then((channel) => {
-        let config = this._getCurrentConfig();
-        if (!isNaN(parseInt(config.channel.prefetch)) && config.channel.prefetch > 0) {
-          channel.prefetch(config.channel.prefetch);
-        }
-        return channel;
-      })
-      .then((channel) => {
-        console.log('emit channel');
-        this.publish();
-        this.emit('channel', this);
-        return channel;
-      });
   }
 
-  _handleError(e) {
-    console.log(JSON.stringify(e));
-    this._tryRecover();
+  getAllChannels() {
+    let channels = [];
+    for (let connectionIndex in this.connections) {
+      if (this.connections[connectionIndex].alive) {
+        for (let channelIndex in this.connections[connectionIndex].channels) {
+          if (this.connections[connectionIndex].channels[channelIndex].alive) {
+            channels.push(this.connections[connectionIndex].channels[channelIndex]);
+          }
+        }
+      }
+    }
+    return channels;
   }
 }
 
-module.exports = AmqpPool;
+module.exports = AMQPPool;
